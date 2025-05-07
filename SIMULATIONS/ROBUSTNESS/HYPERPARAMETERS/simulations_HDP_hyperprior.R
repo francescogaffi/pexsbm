720d48911ede179c6774be938b3439121b73960a
Daniele Durante <daniele.durante@unibocconi.it>
2025-05-07 09:45:05 +0200

    Add files via upload

---
+++ SIMULATIONS/ROBUSTNESS/HYPERPARAMETERS/simulations_HDP_hyperprior.R
@@ -0,0 +1,111 @@
+# load useful libraries
+library(RColorBrewer)
+library(igraph)
+library(pheatmap)
+library(gridExtra)
+library(grid)
+library(copula)
+library(mcclust.ext)
+library(reshape)
+library(gdata)
+library(igraph)
+library(lattice)
+library(cowplot)
+library(ggplot2)
+library(coda)
+library(greed)
+library(LaplacesDemon)
+library(tidyverse)
+library(pracma)
+library(dummy)
+
+# remove all except for functions
+rm(list = setdiff(ls(), lsf.str()))
+
+# load source functions
+source("pex-sbm.R")
+source("numcl.R")
+
+##########################################################################
+####### SIMULATION SCENARIOS FOR PEX-SBM WITH HPD HYPERPRIOR #############
+##########################################################################
+
+load("simulations.Rdata")
+
+# setting the dimensions and other useful quantities
+n_iter  <- 10000
+burn_in <- 2000
+V <- 80
+N  <- c(30,30,15,5)
+
+# HPD hyperprior parameters setting
+alpha   <- 12
+lambda  <- 6
+alpha0  <- 14
+lambda0 <- 2
+expnumcl_hdpgamma(N,alpha, lambda, alpha0, lambda0,2000)
+
+# define the matrices containing the performance measures
+waic          <- matrix(0,2,10)
+vi            <- matrix(0,2,10)
+expected_vi   <- matrix(0,2,10)
+vi_bound      <- matrix(0,2,10)
+z_estimated   <- array(0,c(2,10,80))
+H_estimated   <- matrix(0,2,10)
+H_posterior   <- array(0,c(2,10,3))
+
+
+for (r in 1:10){
+	
+Y1 <- Y1_replicates[,,r]
+Y2 <- Y2_replicates[,,r]
+
+#---------------------------------------------------------------
+# fit pEx-SBM(HDP hyperprior) to scenario 1
+set.seed(r)
+model1       <- hdpgamma_sbm(Y1,n_iter,N,1,1,alpha,lambda,alpha0,lambda0)
+z_post1      <- model1[[1]]
+P_post1      <- pr_cc(z_post1)
+z_hat1       <- minVI(P_post1,method="avg",max.k=20)$cl
+
+vi_bound[1,r] <- credibleball(z_hat1,t(z_post1[,(burn_in+1):n_iter]))[[5]]
+expected_vi[1,r] <- VI(z,t(z_post1[,(burn_in+1):n_iter]))
+vi[1,r] <- VI(z,t(z_hat1))
+H_estimated[1,r] <- max(z_hat1)
+H_posterior[1,r,] <- summary(apply(z_post1[,(burn_in+1):n_iter],2,max))[c(2,3,5)]
+z_estimated[1,r,] <- z_hat1
+
+LL <- matrix(nrow=V*(V-1)/2,ncol=n_iter-burn_in)
+z_waic <- z_post1[,(burn_in+1):n_iter]
+for (t in 1:dim(z_waic)[2]){
+  LL[,t]<-sampleLL(z_waic[,t],Y1,1,1)
+  if (t%%1000 == 0){print(paste("Iteration:", t))}
+}
+waic[1,r] <- WAIC(LL)$WAIC
+
+#---------------------------------------------------------------
+# fit pEx-SBM(HDP hyperprior) to scenario 2
+set.seed(r)
+model2       <- hdpgamma_sbm(Y2,n_iter,N,1,1,alpha,lambda,alpha0,lambda0)
+z_post2      <- model2[[1]]
+P_post2      <- pr_cc(z_post2)
+z_hat2       <- minVI(P_post2,method="avg",max.k=20)$cl
+
+vi_bound[2,r] <- credibleball(z_hat2,t(z_post2[,(burn_in+1):n_iter]))[[5]]
+expected_vi[2,r] <- VI(z,t(z_post2[,(burn_in+1):n_iter]))
+vi[2,r] <- VI(z,t(z_hat2))
+H_estimated[2,r] <- max(z_hat2)
+H_posterior[2,r,] <- summary(apply(z_post2[,(burn_in+1):n_iter],2,max))[c(2,3,5)]
+z_estimated[2,r,] <- z_hat2
+
+LL <- matrix(nrow=V*(V-1)/2,ncol=n_iter-burn_in)
+z_waic <- z_post2[,(burn_in+1):n_iter]
+for (t in 1:dim(z_waic)[2]){
+  LL[,t]<-sampleLL(z_waic[,t],Y2,1,1)
+  if (t%%1000 == 0){print(paste("Iteration:", t))}
+}
+waic[2,r] <- WAIC(LL)$WAIC
+
+print(r)}
+
+save(waic, vi, expected_vi, vi_bound, z_estimated, H_estimated, H_posterior,file="output_HDP_hyperprior.RData")
